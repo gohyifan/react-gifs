c0204c287fa8588559b53eb9250eebb543de41db
Yi Fan Goh <gohyf91@gmail.com>
2020-03-09 20:20:13 +0800

    Completed my first react app

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import giphy from 'giphy-api';
+
+import SearchBar from './components/search_bar';
+import GifList from './components/gif_list';
+import Gif from './components/gif';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGifId: "a93jwI0wkWTQs"
+    };
+
+    this.search('homer');
+  }
+
+  search = (query) => {
+    giphy('6ArxhXWZnk5SPNyJ4TTK7chrN1f2UXGG').search(query, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />, root);
 }
